df7eb805008c5850be64e43641c2cc988e985dfb
kramstyles <mark.eke@decagon.dev>
2022-04-11 00:53:27 +0100

    Added test package and wrote models test

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class TestModels(TestCase):
+    def setUp(self):
+        self.email = 'me@email.com'
+        self.password = '1234pass'
+    
+    def test_if_user_creates_with_email(self):
+        """Test to check if an account can be created with an email address"""
+        user = get_user_model().objects.create_user(email=self.email, password=self.password)
+
+        self.assertEqual(user.email, self.email)
+        self.assertTrue(user.check_password(self.password))
